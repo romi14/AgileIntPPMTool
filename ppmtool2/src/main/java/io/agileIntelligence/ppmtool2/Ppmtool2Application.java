2f7c1b703405200584695dc9fb59d37ecb59134e
romi14 <romikalra96@gmail.com>
2021-04-20 11:56:22 -0700

    first commit

---
+++ ppmtool2/src/main/java/io/agileIntelligence/ppmtool2/Ppmtool2Application.java
@@ -0,0 +1,13 @@
+package io.agileIntelligence.ppmtool2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ppmtool2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ppmtool2Application.class, args);
+	}
+
+}
---
+++ ppmtool2/src/test/java/io/agileIntelligence/ppmtool2/Ppmtool2ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.agileIntelligence.ppmtool2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ppmtool2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
